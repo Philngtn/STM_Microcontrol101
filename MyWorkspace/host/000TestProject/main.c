448accf949eb74c9231a678347cf873a2aadd571
PhilLaptop <philngtn@gmail.com>
2024-07-05 15:26:31 -0400

    first commit

---
+++ MyWorkspace/host/000TestProject/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: Jun 30, 2024
+ *      Author: philn
+ */
+
+#include<stdio.h>
+
+int main(void){
+	printf("Hello World\n");
+	printf("Press 'Enter' to exit this application\n");
+
+	getchar();
+	return 0;
+}
